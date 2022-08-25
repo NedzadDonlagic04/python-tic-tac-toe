4540f7641082d8584857089c4f9b5a811d30e6a1
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-08-25 16:04:49 +0200

    Added player name input, spot choice validation

--- main.py
+++ main.py
@@ -3,30 +3,66 @@ def introduction():
     print('The game is simple, place either X or O on a tile by entering the tile number')
     print('Tile numbers go from 1 to 9 going from the top left to the bottom right')
     print('Do you wish to play single-player, multi-player or view the scoreboard?')
-        
+
+def whiteSpaceCount(list):
+    count = 0
+    for i in range(0,9):
+        if list[i] == ' ':
+            count += 1
+    return count
+
+def getPlayerName(playerNum):
+    return input(f'Player {playerNum} enter your name: ')
+
+def spotValidation(board,spot):
+    try:
+        spot = int(spot) - 1
+    except:
+        return False
+
+    if spot < 0 or spot > 8:
+        return False
+    elif board[spot] != ' ':
+        return False
+    return True
+    
 
 def main():
     introduction()
     choice = input('Enter S, M or V: ')
     
-    spots = [' '] * 9
+    board = [' '] * 9
+    sign = None
+    spot = None
 
-    if choice == 'S':
-        print('Single player!')
-    elif choice == 'M':
-        print('Multi player')
+    if choice == 'S' or choice == 'M':
+        player1 = getPlayerName(1)
+        player2 = None
+
+        while True:
+            player2 = getPlayerName(2) if choice == 'M' else 'Computer'
+            if player2 == player1:
+                print('Players must have different names!')
+            else:
+                break
+
+        sign = 'X'
+        while True:
+            if sign == 'X':
+                spot = input(f'{player1} choose where to place your X: ')
+            else:
+                spot = input(f'{player2} choose where to place your O: ')
+
+            print(spotValidation(board,spot))
+            sign = 'X' if sign == 'O' else 'O'
+
+            if whiteSpaceCount(board) == 0:
+                print("It's a tie!")
+            
     elif choice == 'V':
         print('Scoreboard')
     else:
         print('Error incorrect input. Game terminated.')
-
-    """
-    spots = [' '] * 9
-    for i in range(0,3):
-        print(f' {spots[i]} | {spots[i+1]} | {spots[i+2]} ')
-        if i != 2:
-            print('------------')
-    """
 
 if __name__ == '__main__':
     main()
