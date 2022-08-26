d4c1d3a8a88320a448a41f4b2ebf7f77b9da7bca
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-08-26 22:59:55 +0200

    Finished the single-player and multi-player modes

--- main.py
+++ main.py
@@ -1,64 +1,57 @@
-def introduction():
-    print('Hello! Welcome to a simple tic tac toe game made with python!')
-    print('The game is simple, place either X or O on a tile by entering the tile number')
-    print('Tile numbers go from 1 to 9 going from the top left to the bottom right')
-    print('Do you wish to play single-player, multi-player or view the scoreboard?')
-
-def whiteSpaceCount(list):
-    count = 0
-    for i in range(0,9):
-        if list[i] == ' ':
-            count += 1
-    return count
-
-def getPlayerName(playerNum):
-    return input(f'Player {playerNum} enter your name: ')
-
-def spotValidation(board,spot):
-    try:
-        spot = int(spot) - 1
-    except:
-        return False
-
-    if spot < 0 or spot > 8:
-        return False
-    elif board[spot] != ' ':
-        return False
-    return True
-    
+import gameFunctions as game
 
 def main():
-    introduction()
+    game.introduction()
     choice = input('Enter S, M or V: ')
     
     board = [' '] * 9
     sign = None
-    spot = None
+    player1 = None
+    player2 = 'Computer'
 
-    if choice == 'S' or choice == 'M':
-        player1 = getPlayerName(1)
-        player2 = None
+    if choice == 'S':
+        player1 = game.getPlayerName(1)
 
-        while True:
-            player2 = getPlayerName(2) if choice == 'M' else 'Computer'
-            if player2 == player1:
-                print('Players must have different names!')
-            else:
-                break
-
+        player = player1
         sign = 'X'
         while True:
             if sign == 'X':
-                spot = input(f'{player1} choose where to place your X: ')
+                game.getPlayerMove(board, player, sign)
             else:
-                spot = input(f'{player2} choose where to place your O: ')
-
-            print(spotValidation(board,spot))
-            sign = 'X' if sign == 'O' else 'O'
-
-            if whiteSpaceCount(board) == 0:
-                print("It's a tie!")
+                game.getComputerMove(board)
             
+
+            if game.whiteSpaceCount(board) == 0:
+                game.boardOutput(board)
+                print('It is a tie!')
+                break
+            elif game.checkWinner(board, sign):
+                game.boardOutput(board)
+                print(f'Player {player} wins!')
+                break
+
+            sign = 'X' if sign == 'O' else 'O'
+            player = player1 if player == player2 else player2
+    elif choice == 'M':
+        player1 = game.getPlayerName(1)
+        player2 = game.getPlayerName(2, player1)
+
+        player = player1
+        sign = 'X'
+        while True:
+            game.getPlayerMove(board, player, sign)
+
+            if game.whiteSpaceCount(board) == 0:
+                game.boardOutput(board)
+                print('It is a tie!')
+                break
+            elif game.checkWinner(board, sign):
+                game.boardOutput(board)
+                print(f'Player {player} wins!')
+                break
+
+            sign = 'X' if sign == 'O' else 'O'
+            player = player1 if player == player2 else player2
     elif choice == 'V':
         print('Scoreboard')
     else:
